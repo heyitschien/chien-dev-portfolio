f2c8e36d071b5d3a8b0fe66689b0b6f9be788430
Chien Escalera Duong <heyitschien@gmail.com>
2025-11-07 14:32:54 -0800

    feat: add cn utility for className management
    - Install clsx and tailwind-merge
    - Create lib/utils.ts with cn() helper function
    - Enables better className merging and conditional classes
    - Ready for use in components (will migrate incrementally)
    - Tested: TypeScript compiles successfully

---
+++ lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * Utility function to merge Tailwind classes with clsx
+ * Handles conditional classes and merges conflicting Tailwind classes
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
